e6d0006afd447852681debb515e61a6a73014522
Brian Shaw <bshaw@hey.com>
2020-06-27 14:13:47 +0100

    Add favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -42,6 +42,7 @@ STATIC_PATHS = [
 
 EXTRA_PATH_METADATA = {
     'extra/custom.css': {'path': 'static/custom.css'},
+    'extra/favicon.ico': {'path': 'favicon.ico'}
 }
 
 CUSTOM_CSS = 'static/custom.css'
